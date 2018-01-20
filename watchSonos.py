b7f5b74763ee42f738114ddf795b73c2922fd4b2
Joshua R. Morain <SchroedingerHepcat@gmail.com>
2018-01-19 21:45:27 -0700

    Only handle transitioning if previously was paused.

--- watchSonos.py
+++ watchSonos.py
@@ -10,16 +10,21 @@ subscription = sonos.group.coordinator.avTransport.subscribe(auto_renew=True)
 
 avr = marantz.Marantz('192.168.1.53')
 
+lastState=''
+
 while True:
     try:
         event = subscription.events.get(timeout=0.5)
         print(event.variables['transport_state'])
 
-        if event.variables['transport_state'] == "PLAYING":
+        if event.variables['transport_state'] == 'PLAYING':
+            lastState = event.variables['transport_state']
             avr.cd()
-        elif event.variables['transport_state'] == "TRANSITIONING":
-            avr.cd()
-        elif event.variables['transport_state'] == "PAUSED_PLAYBACK":
+        elif event.variables['transport_state'] == 'TRANSITIONING':
+            if lastState == 'PAUSED_PLAYBACK':
+                avr.cd()
+        elif event.variables['transport_state'] == 'PAUSED_PLAYBACK':
+            lastState = event.variables['transport_state']
             avr.tv()
 
     except queue.Empty:
