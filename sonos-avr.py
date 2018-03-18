2cc9a93f3f6d9f562678176e0b4b04ac8481fd1e
SchroedingerHepcat <SchroedingerHepcat@gmail.com>
2018-03-18 15:16:59 -0600

    Add comments and a logfile.

--- sonos-avr.py
+++ sonos-avr.py
@@ -7,16 +7,8 @@ import marantz
 import sys
 import logging
 
-logging.basicConfig(level="INFO")
-
-sonos = soco.SoCo('192.168.1.20')
-subscription = sonos.group.coordinator.avTransport.subscribe(auto_renew=True)
-groupSubscription = sonos.zoneGroupTopology.subscribe()
-
-avr = marantz.Marantz('192.168.1.53')
-lastState=''
-
 def shutdown(signal, frame):
+    """ Function to handle shutting down on SIGINT """
     logging.info('shutdown(): Shutting down...')
     subscription.unsubscribe()
     groupSubscription.unsubscribe()
@@ -24,10 +16,35 @@ def shutdown(signal, frame):
 
 signal.signal(signal.SIGINT, shutdown)
 
+
+# Set a log level and file
+logging.basicConfig(filename="sonos-avr.log", level="INFO")
+
+# Connect to the desired Sonos Connect (i.e. the one connected to the AVR)
+sonos = soco.SoCo('192.168.1.20')
+
+# Subscribe to playback events
+# This subscribes to the group coordinator for the Sonos Connect, which may or
+# may not be the Connect itself, depending on if the speakers are grouped
+subscription = sonos.group.coordinator.avTransport.subscribe(auto_renew=True)
+
+# Subscribe to events about how the Connect is grouped
+groupSubscription = sonos.zoneGroupTopology.subscribe()
+
+# Connect to the Marantz AVR
+avr = marantz.Marantz('192.168.1.53')
+
+# Initialize a few things
+lastState=''
+
+# Main event loop
 while True:
     logging.debug("main(): Main loop: Start...")
+
     # Check zone event subscription
-    if not groupSubscription.is_subscribed or groupSubscription.time_left <= 5:
+    if (  not groupSubscription.is_subscribed
+       or groupSubscription.time_left <= 5
+       ):
         try:
             logging.info("Unsubscribing to zone events")
             groupSubscription.unsubscribe()
@@ -54,12 +71,13 @@ while True:
 
         try:
             subscription = sonos.group.coordinator.avTransport.subscribe(
-                                                                auto_renew=True
+                                                               auto_renew=True
                                                                         )
         except Exception as e:
             logging.warning('Playback event subscription failed.')
             time.sleep(10)
 
+    # Check for grouping events and change playback subscription if necessary
     try:
         logging.debug("Checking zone events...")
         zoneEvent = None
@@ -73,8 +91,10 @@ while True:
                                                                 auto_renew=True
                                                                             )
     except queue.Empty:
+        # If there are no events, move on
         pass
 
+    # Check for playback events and change AVR as needed
     try:
         logging.debug("Checking playback events...")
         playEvent = None
@@ -87,7 +107,7 @@ while True:
                 lastState = playEvent.variables['transport_state']
                 avr.cd()
             elif playEvent.variables['transport_state'] == 'TRANSITIONING':
-                if lastState == 'PAUSED_PLAYBACK':
+                if lastState == 'PAUSED_PLAYBACK' or lastState == 'STOPPED':
                     avr.cd()
             elif playEvent.variables['transport_state'] == 'PAUSED_PLAYBACK':
                 lastState = playEvent.variables['transport_state']
@@ -96,4 +116,5 @@ while True:
                 lastState = playEvent.variables['transport_state']
                 avr.tv()
     except queue.Empty:
+        # If there are no events, move on
         pass
